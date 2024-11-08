23c2e13fc7992c644da214254156ddb0a57740d9
Sergio Akira Sarui Junior <sergiosaruijr@gmail.com>
2024-11-08 01:01:22 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-500">Hello world</h1>
+    </div>
   );
 }
